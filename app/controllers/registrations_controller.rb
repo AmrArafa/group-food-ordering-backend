d7db3db6a80efade8fe6f8fdb0215d79a5c6071a
Amr Arafa <amr.arafa129@gmail.com>
2018-01-04 17:21:24 +0200

    Added @user.errors.full_messages

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,7 +9,7 @@ def new
     if @user.save
       render json: @user, status: :created, location: @user
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: @user.errors.full_messages, status: :unprocessable_entity
     end
   end
 
