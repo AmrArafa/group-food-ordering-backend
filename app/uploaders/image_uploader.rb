ddd2a9ffd807804425b4ac564fd44b8bcde6dd96
anadoly <ahmedelanadoly@gmail.com>
2018-01-14 13:39:35 +0200

    Image Uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
