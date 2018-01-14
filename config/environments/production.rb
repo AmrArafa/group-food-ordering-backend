ae6f8e799229f16867d964fc7ae3728dfcf0dc23
anadoly <ahmedelanadoly@gmail.com>
2018-01-14 12:23:09 +0200

    delete domin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'ordering-food-app.herokuapp.com',
   user_name:            ENV['GMAIL_USERNAME'],
   password:             ENV['GMAIL_PASSWORD'],
   authentication:       'plain',
