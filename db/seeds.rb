70d3673fe632770c9d9f53e2898e065fea7bcd8d
anadoly <ahmedelanadoly@gmail.com>
2018-01-15 16:12:13 +0200

    Fix db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,46 +6,46 @@
 # # #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 # # #   Character.create(name: 'Luke', movie: movies.first)
 
-Item.create(
-    :name => 'Chicken spring roll with soy sauce and sweet chili sauce',
-    :image => File.open(File.join(Rails.root, '/image/Chicken_spring_roll_with_soy_sauce_and_sweet_chili_sauce.jpg')),
-    :price => 46.00
-    )
-Item.create(
-    :name => 'Chicken breaded kofta with pasta red sauce',
-    :image => File.open(File.join(Rails.root, '/image/Chicken_breaded_kofta_with_pasta_red_sauce.jpg')),
-    :price => 59.00
-    )
-Item.create(
-    :name => 'Boneless chicken thigh with potato pure',
-    :image => File.open(File.join(Rails.root, '/image/Boneless_chicken_thigh_with_potato_pure.jpg')),
-    :price => 68.00
-    )
-Item.create(
-    :name => 'Bechamil pasta with pane',
-    :image => File.open(File.join(Rails.root, '/image/Bechamil_pasta_with_pane.jpg')),
-    :price => 68.00
-    )
-Item.create(
-    :name => 'Chicken Mushroom & Lemon Sauce, with Rice',
-    :image => File.open(File.join(Rails.root, '/image/Chicken_Mushroom_&_Lemon_Sauce_with_Rice.jpg')),
-    :price => 57.00
-    )
-Item.create(
-    :name => 'Bechamil pasta with green salad',
-    :image => File.open(File.join(Rails.root, '/image/Bechamil_past_with_green_salad.jpg')),
-    :price => 73.00
-    )
-Item.create(
-    :name => 'Zucchini bechamel with white rice',
-    :image => File.open(File.join(Rails.root, '/image/Zucchini_bechamel_with_white_rice.jpg')),
-    :price => 55.00
-    )
-Item.create(
-    :name => 'Meat mombar with baba ghanouj',
-    :image => File.open(File.join(Rails.root, '/image/Meat_mombar_with_baba_ghanouj.jpg')),
-    :price => 61.00
-    )
+# Item.create(
+#     :name => 'Chicken spring roll with soy sauce and sweet chili sauce',
+#     :image => File.open(File.join(Rails.root, '/image/Chicken_spring_roll_with_soy_sauce_and_sweet_chili_sauce.jpg')),
+#     :price => 46.00
+#     )
+# Item.create(
+#     :name => 'Chicken breaded kofta with pasta red sauce',
+#     :image => File.open(File.join(Rails.root, '/image/Chicken_breaded_kofta_with_pasta_red_sauce.jpg')),
+#     :price => 59.00
+#     )
+# Item.create(
+#     :name => 'Boneless chicken thigh with potato pure',
+#     :image => File.open(File.join(Rails.root, '/image/Boneless_chicken_thigh_with_potato_pure.jpg')),
+#     :price => 68.00
+#     )
+# Item.create(
+#     :name => 'Bechamil pasta with pane',
+#     :image => File.open(File.join(Rails.root, '/image/Bechamil_pasta_with_pane.jpg')),
+#     :price => 68.00
+#     )
+# Item.create(
+#     :name => 'Chicken Mushroom & Lemon Sauce, with Rice',
+#     :image => File.open(File.join(Rails.root, '/image/Chicken_Mushroom_&_Lemon_Sauce_with_Rice.jpg')),
+#     :price => 57.00
+#     )
+# Item.create(
+#     :name => 'Bechamil pasta with green salad',
+#     :image => File.open(File.join(Rails.root, '/image/Bechamil_past_with_green_salad.jpg')),
+#     :price => 73.00
+#     )
+# Item.create(
+#     :name => 'Zucchini bechamel with white rice',
+#     :image => File.open(File.join(Rails.root, '/image/Zucchini_bechamel_with_white_rice.jpg')),
+#     :price => 55.00
+#     )
+# Item.create(
+#     :name => 'Meat mombar with baba ghanouj',
+#     :image => File.open(File.join(Rails.root, '/image/Meat_mombar_with_baba_ghanouj.jpg')),
+#     :price => 61.00
+#     )
 Admin.create(
     :first_name => 'Bahia',
     :last_name => 'El Sharkawy',
